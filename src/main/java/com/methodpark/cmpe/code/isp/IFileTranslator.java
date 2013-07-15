e0731929868d0ca84cb5731f5a41509ce0c5a315
adl <adl@methodpark.de>
2013-07-15 22:39:47 +0200

    added problem anlaysis to isp example

--- src/main/java/com/methodpark/cmpe/code/isp/IFileTranslator.java
+++ src/main/java/com/methodpark/cmpe/code/isp/IFileTranslator.java
@@ -1,5 +1,7 @@
 package com.methodpark.cmpe.code.isp;
 
+// we need two separate interfaces: ITranslator and ITextFromFileReader
+// if we really need a class that implements both interfaces, we can do so, but this is not the case in main. 
 public interface IFileTranslator
 {
     public abstract String translate(String text);
